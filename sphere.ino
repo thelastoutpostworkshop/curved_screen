3d8ae3b360d2a83a499ced891c5a580eec197162
charles <cgiguere@iname.com>
2024-02-21 06:16:58 -0500

    removed unused code

--- sphere.ino
+++ sphere.ino
@@ -5,9 +5,6 @@
 #define FRAME_BUFFER_SIZE 50000L
 uint8_t *frameBuffer;
 
-uint8_t *fileBuffer = NULL;
-size_t fileBufferSize = 0;
-
 typedef struct
 {
     int csPin;
@@ -28,25 +25,6 @@ void createDisplay(void)
     for (int i = 0; i < SCREEN_COUNT; i++)
     {
         grid[i].display = new Display(grid[i].csPin, grid[i].rotation);
-    }
-}
-
-void getFrames(void)
-{
-    int framesCount = getFramesCount();
-    Serial.printf("Frames Count = %d\n", framesCount);
-
-    for (int i = 0; i < SCREEN_COUNT; i++)
-    {
-        Screen currentScreen = grid[i];
-        for (int frameIndex = 0; frameIndex < framesCount; frameIndex++)
-        {
-            u_int8_t *frame = currentScreen.display->addNewFrame();
-            if (frame != NULL)
-            {
-                getFrameData(i, frameIndex, frame, currentScreen.display->getFrameSize());
-            }
-        }
     }
 }
 
@@ -73,8 +51,6 @@ bool getJPGFrames(void)
     return true;
 }
 
-int frameCount;
-
 void setup()
 {
     Serial.begin(115200);
@@ -98,18 +74,10 @@ void setup()
             ;
     }
 
-    // frameCount = getFramesCount();
-    // Serial.printf("Frames Count = %d\n", frameCount);
-    // for (int i = 0; i < SCREEN_COUNT; i++)
-    // {
-    //     grid[i].display->addNewFrame();
-    // }
     Serial.printf("PSRAM left = %lu\n", ESP.getFreePsram());
 }
 
-int currentFrame = 0;
 unsigned long t;
-
 void loop()
 {
     t = millis();
@@ -118,40 +86,4 @@ void loop()
         grid[i].display->showJPGFrames();
     }
     Serial.printf("Took %ld ms\n", millis() - t);
-
-    // for (int i = 0; i < SCREEN_COUNT; i++)
-    // {
-    //     grid[i].display->imageSize = getFrameJPGData(i, currentFrame, grid[i].display->getFrame(0), grid[i].display->getFrameSize());
-    //     currentFrame++;
-    //     if (currentFrame == frameCount)
-    //     {
-    //         currentFrame = 0;
-    //     }
-    // }
-
-    // for (int i = 0; i < SCREEN_COUNT; i++)
-    // {
-    //     jpeg.openRAM(grid[i].display->getFrame(0), grid[i].display->imageSize, draw);
-    //     grid[i].display->activate();
-    //     tft.setRotation(grid[i].display->screenRotation);
-    //     while (jpeg.decode(0, 0, 0) == JPEG_SUCCESS)
-    //     {
-    //         /* code */
-    //     }
-    //     grid[i].display->deActivate();
-    //     jpeg.close();
-    // }
-
-    // t = millis();
-
-    // getFrameData(0, currentFrame, grid[0].display->getFrame(0), grid[0].display->getFrameSize());
-    // currentFrame++;
-    // if (currentFrame == frameCount)
-    // {
-    //     currentFrame = 0;
-    // }
-
-    // grid[0].display->showFrame(0);
-
-    // Serial.printf("Took %ld ms\n", millis() - t);
 }
--- webserver.h
+++ webserver.h
@@ -7,6 +7,8 @@ AsyncWebServer server(80);
 HTTPClient http;
 
 const String apiEndpoint = "http://192.168.1.90:3000/api/";
+const String apiFrameCount = "frames-count";
+const String apiFrameJPG = "framejpg/";
 
 void initWebServer()
 {
@@ -32,75 +34,28 @@ void initWebServer()
 
 int getFramesCount()
 {
-    String url = apiEndpoint + "frames-count";
+    String url = apiEndpoint + apiFrameCount;
     http.begin(url);
     int httpCode = http.GET();
 
     if (httpCode > 0)
     {
         String payload = http.getString();
-        http.end();             // Free resources
-        return payload.toInt(); // Convert payload to integer and return
+        http.end();             
+        return payload.toInt(); 
     }
     else
     {
         Serial.println("Error on HTTP request");
-        http.end(); // Free resources
-        return -1;  // Indicate failure
+        http.end(); 
+        return -1;  
     }
 }
 
-int getFrameData(int screenNumber, int frameNumber, uint8_t *buffer, size_t bufferSize)
-{
-    HTTPClient http;
-
-    if (!buffer)
-    {
-        return -1; // Invalid buffer
-    }
-
-    // Ensure variables are converted to String for URL construction
-    String url = apiEndpoint + "frame/" + String(screenNumber) + "/" + String(frameNumber);
-    // Serial.printf("Calling %s\n", url.c_str());
-
-    http.begin(url);           // Start the connection
-    int httpCode = http.GET(); // Make the GET request
-
-    if (httpCode == HTTP_CODE_OK)
-    {
-        WiFiClient *stream = http.getStreamPtr();
-        size_t totalBytesRead = 0;
-        // Loop to read bytes as long as there's data available and buffer has space
-        while (totalBytesRead < bufferSize)
-        {
-            size_t availableBytes = stream->available();
-            if (availableBytes > 0)
-            {
-                int bytesRead = stream->readBytes(buffer + totalBytesRead, min(availableBytes, bufferSize - totalBytesRead));
-                totalBytesRead += bytesRead;
-            }
-            else if (!http.connected())
-            {
-                break; // Break if no longer connected
-            }
-        }
-        http.end(); // End connection
-        // Serial.printf("Total bytes read: %lu\n", totalBytesRead);
-        return totalBytesRead; // Return total bytes read
-    }
-    else
-    {
-        Serial.printf("[HTTP] GET failed, error: %s\n", http.errorToString(httpCode).c_str());
-        http.end(); // Ensure connection is closed
-        return -2;  // HTTP error or no data read
-    }
-}
-
-#include <HTTPClient.h>
 
 size_t getFrameJPGData(int screenNumber, int frameNumber, uint8_t *buffer, size_t bufferSize)
 {
-    String url = apiEndpoint + "framejpg/" + String(screenNumber) + "/" + String(frameNumber);
+    String url = apiEndpoint + apiFrameJPG + String(screenNumber) + "/" + String(frameNumber);
     Serial.printf("Calling %s\n", url.c_str());
 
     http.setReuse(true);
