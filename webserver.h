e6a9bf6959bdc0638f66f07005d0bc5a1abeeca3
charles <cgiguere@iname.com>
2024-02-25 17:44:39 -0500

    removed udp

--- webserver.h
+++ webserver.h
@@ -3,14 +3,8 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <HTTPClient.h>
-#include <WiFiUdp.h>
 #include "secrets.h"
 #include "sphere.h"
-
-WiFiUDP udp;
-unsigned int localUdpPort = 4210;                 // Local port to listen on
-char incomingPacket[255];                         // Buffer for incoming packets
-const char *broadcastAddress = "255.255.255.255"; // Broadcast address
 
 #ifdef MASTER
 AsyncWebServer masterServer(80);
@@ -21,28 +15,6 @@ void handleReady(AsyncWebServerRequest *request)
     request->send(200, "text/plain", "ok");
     slavesReady++;
 }
-void waitForSlavesToshowFrame(void)
-{
-    slavesReady = 0;
-    while (slavesReady < SLAVECOUNT)
-    {
-        int packetSize = udp.parsePacket();
-        if (packetSize)
-        {
-            // Read the packet into the buffer
-            int len = udp.read(incomingPacket, 255);
-            if (len > 0)
-            {
-                incomingPacket[len] = 0; // Null-terminate the string
-                if (strcmp(incomingPacket, "ready") == 0)
-                {
-                    slavesReady++;
-                    Serial.println("Slave ready received");
-                }
-            }
-        }
-    }
-}
 #endif
 
 HTTPClient http;
@@ -50,48 +22,6 @@ HTTPClient http;
 const String apiEndpoint = "http://192.168.1.90:3000/api/";
 const String apiFrameCount = "frames-count";
 const String apiFrameJPG = "framejpg/";
-
-bool waitForCommand(const char *command)
-{
-    int packetSize = udp.parsePacket();
-    if (packetSize)
-    {
-        // read the packet into the buffer
-        int len = udp.read(incomingPacket, sizeof(incomingPacket));
-        if (len > 0)
-        {
-            incomingPacket[len] = 0; // Null-terminate the string
-        }
-        // Serial.printf("Received: %s\n", incomingPacket);
-        if (strcmp(incomingPacket, command) == 0)
-        {
-            return true; // Command matches the expected command
-        }
-    }
-    return false; // No matching command received
-}
-
-void broadcastCommand(const char *command)
-{
-    udp.beginPacket(broadcastAddress, localUdpPort);
-    udp.write((uint8_t *)command, strlen(command)); // Correctly send the command as bytes
-    if (udp.endPacket())
-    {
-        // Serial.printf("Command \"%s\" broadcasted successfully.\n", command);
-    }
-    else
-    {
-        Serial.println("Error broadcasting command.");
-    }
-}
-
-void sendMessageToServer(const char *message)
-{
-    String url = String("http://") + String(SERVERNAME) + ".local";
-    udp.beginPacket(url.c_str(), localUdpPort);
-    udp.write((uint8_t *)message, strlen(message));
-    udp.endPacket();
-}
 
 ErrorCode initWebServer()
 {
@@ -107,9 +37,7 @@ ErrorCode initWebServer()
     Serial.print("IP Address: ");
     Serial.println(WiFi.localIP());
 
-    udp.begin(localUdpPort);
-    Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
-#ifdef MASTER
+ #ifdef MASTER
     if (!MDNS.begin(SERVERNAME))
     {
         Serial.println("Error starting mDNS for the Master");
