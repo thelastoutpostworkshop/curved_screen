8dd6841c04a9b8ab5b7f7e363cb23cbfe2ee9a7a
charles <cgiguere@iname.com>
2024-03-10 13:29:21 -0400

    setConnectTimeout

--- webserver.h
+++ webserver.h
@@ -238,7 +238,7 @@ uint8_t *getGifData(String esp_id, int screenNumber, size_t *bufferSize)
 
     String url = apiEndpoint + apiGif + esp_id + "/" + String(screenNumber);
     void *gifData = NULL;
-
+    http.setConnectTimeout(10000);
     httpCode = HTTPGetWithRetry(&http, url, httpCode);
     if (httpCode == HTTP_CODE_OK)
     {
