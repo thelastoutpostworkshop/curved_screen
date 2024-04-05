bae2457a59e8f0ce5fc763e48f2587544e0d79a1
charles <cgiguere@iname.com>
2024-04-05 16:13:10 -0400

    3 screens

--- sphere.ino
+++ sphere.ino
@@ -15,13 +15,18 @@ typedef struct
     Display *display;
 } Screen;
 
-#define SCREEN_COUNT 4
+#define SCREEN_COUNT 3
+
+// Screen grid[SCREEN_COUNT] = {
+//     {.csPin = 17, .rotation = 3},
+//     {.csPin = 18, .rotation = 3},
+//     {.csPin = 47, .rotation = 3},
+//     {.csPin = 48, .rotation = 3}};
 
 Screen grid[SCREEN_COUNT] = {
     {.csPin = 17, .rotation = 3},
     {.csPin = 18, .rotation = 3},
-    {.csPin = 47, .rotation = 3},
-    {.csPin = 48, .rotation = 3}};
+    {.csPin = 47, .rotation = 3}};
 
 Calibration calibration;
 
