bc2f41283bd15e2996a42924e6b8ff8c62d2ea6e
charles <cgiguere@iname.com>
2025-01-12 08:22:00 -0500

    _SYNC_

--- sync.h
+++ sync.h
@@ -1,10 +1,9 @@
-#ifndef _SPHERE_
-#define _SPHERE_
+#ifndef _SYNC_
+#define _SYNC_
 #include "slaves.h"
 
 #define SERVERNAME "sphere"
 #define PIN_SYNC_SHOW_FRAME 38
-
 
 #ifdef MASTER
 SLAVES slaves;
