4da090bd1ccdfd3f289a80f1c07a1b0ab51e4cbc
charles <cgiguere@iname.com>
2025-01-18 08:41:41 -0500

    moved define

--- slaves.h
+++ slaves.h
@@ -3,8 +3,6 @@
 #ifndef _SLAVES_
 #define _SLAVES_
 
-#define SLAVECOUNT 1 // The number of ESP32-S3 slaves
-
 class SLAVES
 {
 private:
--- sphere.ino
+++ sphere.ino
@@ -5,6 +5,8 @@
 // Uncomment the next line to compile and upload the code to the ESP32-S3 acting as the master - only one master is allowed
 // #define MASTER  
 
+#define SLAVECOUNT 1 // The number of ESP32-S3 slaves
+
 #include "sync.h"
 #include "displayRaw.h"
 #include "webserver.h"
